30cf925a99f393a79805f61083af516cbe82579c
Cha <mike.klingbiel@gmx.net>
2016-07-04 15:31:37 +0200

    added GameWorld Class and the GameComand abstract class

---
+++ GameCommand.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "World.h"
+
+namespace brk
+{
+	class GameCommand
+	{
+	public:
+		virtual ~GameCommand() {}
+		virtual World operator()(World& world);
+	};
+}
---
+++ World.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Map.h"
+
+namespace brk {
+
+	class World
+	{
+	private:
+		brkr::Map gameMap;
+	public:
+		World();
+
+
+
+
+	};
+
+}
